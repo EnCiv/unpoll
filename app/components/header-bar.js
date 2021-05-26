4bebceac741cac0831967fc58e460c955c2be9ec
Thong Pham <phminhthong88@gmail.com>
2021-05-25 23:58:32 -0700

    saving

--- app/components/header-bar.js
+++ app/components/header-bar.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import {createUseStyles} from 'react-jss';
 import SvgAppIcon from '../svgr/app-icon';
 import SvgUndoIcon from '../svgr/undo-icon';
@@ -36,13 +36,36 @@ const useStyles = createUseStyles({
     },
     dotIcon: {
         marginLeft: '1rem'
+    },
+    pointer: {
+        cursor: 'pointer',
+    },
+    disabledIcon: {
+        cursor: 'no-drop',
+        opacity: '0.65'
     }
 })
 
 export function HeaderBar(props) {
     const { type, navSteps, currentStep } = props;
     const classes = useStyles();
+    const [backActive, updateBackButton] = useState(true);
+    const [activeStep, updateStep] = useState(currentStep)
     const steps = [];
+
+    function onBackButton () {
+        console.log("onBackButton callled");
+        if (activeStep > 1) {
+            updateStep(activeStep - 1);
+        } else {
+            updateBackButton(false);
+        }
+    }
+
+    function onRedoButton () {
+        console.log("onRedoButton callled");
+    }
+
     for (let i = 1; i <= navSteps; i++) {
         steps.push(i);
     }
@@ -59,16 +82,16 @@ export function HeaderBar(props) {
                 <div className={classes['right']}></div>
             </div> : 
             <div className={classes['headerBar']}>
-                <div className={classes['left']}>
-                    <SvgBackIcon width="30px" height="30px" />
+                <div className={classes['left']} >
+                    <SvgBackIcon width="30px" height="30px" className={backActive ? classes.pointer : classes.disabledIcon} onClick={onBackButton} />
                 </div>
                 <div className={classes['middle']}>
                     {steps.map((step, index) => 
-                        (step === currentStep) ? <SvgWhiteDotIcon key={index} className={classes['dotIcon']} /> : <SvgBlackDotIcon key={index} className={classes['dotIcon']} />
+                        (step === activeStep) ? <SvgWhiteDotIcon key={index} className={classes.dotIcon} /> : <SvgBlackDotIcon key={index} className={classes['dotIcon']} />
                     )}
                 </div>
                 <div className={classes['right']}>
-                    <SvgUndoIcon width="30px" height="30px" />
+                    <SvgUndoIcon width="30px" height="30px" className={classes.pointer} onClick={onRedoButton} />
                 </div>
             </div>}
         </Fragment>
