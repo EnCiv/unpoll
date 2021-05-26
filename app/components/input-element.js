2aea832f6013b84022d76d8e132e88073dc659f0
Thong Pham <phminhthong88@gmail.com>
2021-05-25 22:51:05 -0700

    saving

---
+++ app/components/input-element.js
@@ -0,0 +1,84 @@
+'use strict'
+
+import React, { useState } from 'react'
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+    inputContainer: {
+        display: 'flex',
+        width: '100%',
+        height: '10rem',
+        backgroundColor: '#000',
+        color: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '2rem',
+    },
+    topicContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+    },
+    topicInput: {
+        border: '2px solid #fff',
+        borderRadius: '10px',
+        padding: '1rem',
+        boxSizing: 'border-box',
+        height: '4rem',
+        width: '100%',
+        backgroundColor: '#000',
+        color: '#fff',
+        '::-webkit-input-placeholder': {
+            color: '#fff'
+        }
+    },
+    topicInputClicked: {
+        border: '2px solid #fff',
+        borderRadius: '10px',
+        padding: '1rem',
+        boxSizing: 'border-box',
+        height: '6rem',
+        width: '100%',
+        backgroundColor: '#000',
+        color: '#fff',
+        '::-webkit-input-placeholder': {
+            color: '#fff'
+        }
+    },
+    topicInputText: {
+        width: '100%',
+        height: '100%',
+        background: 'transparent',
+        color: '#fff',
+        border: 'none',
+        outline: 'none',
+        fontSize: '1.25rem',
+        lineHeight: '1.25rem',
+    },
+    topicInputTitle: {
+        textAlign: 'left'
+    }
+})
+
+
+export function InputElement() {
+    const classes = useStyles()
+    const [ inputClicked, changeInput ] = useState(false)
+    return (
+        <div className={classes.inputContainer}>
+            <div className={classes.topicContainer}>
+                {!inputClicked ? 
+                    <div className={classes.topicInput}>
+                        <input type='text' className={classes.topicInputText} placeholder={`TOPIC 1`} onClick={() => changeInput(true)} />
+                    </div> :
+                    <div className={classes.topicInputClicked}>
+                        <div className={classes.topicInputTitle}>TOPIC 1</div>
+                        <input type='text' className={classes['topicInputText']} />
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default InputElement
---
+++ stories/input-element.stories.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { InputElement } from '../app/components/input-element';
+
+const Component = InputElement
+const Name = 'InputElement'
+
+export default {
+    title: Name,
+    component: Component,
+    argTypes: {},
+}
+
+const Template = args => (
+    <div style={{ width: '100vw', height: '100vh' }}>
+        <div
+            style={{
+                width: '48em',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                textAlign: 'center',
+                padding: '1rem',
+                backgroundColor: '#fff',
+                height: '100vh',
+            }}
+        >
+            <Component {...args}>
+                <InputElement />
+            </Component>
+        </div>
+    </div>
+)
+
+export const InputBox = Template.bind({})
+InputBox.args = {}
