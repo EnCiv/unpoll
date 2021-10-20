2382e5bbe1ed2dc7ffabfe34b360e5d6a1327e3f
David Fridley <ddfridley@yahoo.com>
2021-10-20 13:54:51 -0700

    civil-client

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -4,22 +4,22 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 import WebComponent from '../web-components'
 import Footer from './footer'
-import ErrorBoundary from '../../node_modules/civil-server/dist/components/error-boundary'
+import { ErrorBoundary } from 'civil-client'
 
-export function App(props){
-      var { iota, ...newProps } = props
-      Object.assign(newProps, iota)
-      return (
-        <ErrorBoundary>
-          <div style={{ position: 'relative' }}>
-            {iota? 
-            <WebComponent key="web-component" webComponent={iota.webComponent} {...newProps} /> :
-            <div>Nothing Here</div>
-            }
-            <Footer />
-          </div>
-        </ErrorBoundary>
-      )
+export function App(props) {
+  var { iota, ...newProps } = props
+  Object.assign(newProps, iota)
+  return (
+    <ErrorBoundary>
+      <div style={{ position: 'relative' }}>
+        {iota ?
+          <WebComponent key="web-component" webComponent={iota.webComponent} {...newProps} /> :
+          <div>Nothing Here</div>
+        }
+        <Footer />
+      </div>
+    </ErrorBoundary>
+  )
 }
 
 export default hot(module)(App)
